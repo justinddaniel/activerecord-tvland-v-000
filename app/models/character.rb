8b57a65519f73bba48797f15b773302ce3dc727f
Justin Daniel <justinddaniel@gmail.com>
2017-10-16 02:53:05 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,13 @@
 class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
+
+  def catchphrase
+    if self.name == "Steve Urkel"
+      urkel.catchphrase = "Did I do that?"
+      urkel.catchphrase
+    else "I am not Steve Urkel and I did not do that"
+    end
+  end
+    
 end
